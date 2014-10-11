4d548cb5c92ad0b9fe8fa9b4bbb5271f82a632cf
guillaumebauchart <guillaume_bauchart@hotmail.com>
2014-10-11 19:59:16 +0200

    Ajout Structures de bases polynomes
    Ajout des structures pour les polynomes, développés, factorisés et
    quelques commentaires.
    Edit de la ToDoList

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ typedef struct
 }Complex;
 
 
+// Définie une structure pour un monome, avec exposant, coefficient, pointeur vers monome suivant et précédent.
 typedef struct Monomial Monomial;
 struct Monomial
 {
@@ -19,12 +20,32 @@ struct Monomial
 };
 
 
-typedef struct Polynomial 
+// Définie une structure pour un polynome développé, avec longueur, degré, pointeur vers le premier et le dernier polynome de la liste, également vers le polynome suivant et précédent dans le cas où il y aurait une liste de molynomes développés (comme dans la forme factorisée par exemple).
+typedef struct Polynomial_dev Polynomial_dev;
+struct Polynomial_dev
 {
     int lenght, deg;
     Monomial *first, *last;
-}Polynomial;
+    Polynomial_dev *next, *prev;
+};
 
+
+// Définie une structure pour un polynome factorisé, avec longueur, degré, pointeur vers le premier et le dernier polynome developpé de la liste.
+typedef struct Polynomial_fact Polynomial_fact;
+struct Polynomial_fact
+{
+    int lenght, deg;
+    Polynomial_dev *first, *last;
+};
+
+
+// Définie une structure pour un polynome avec la longueur, le degré, la forme développée, intégrée, dérivée, factorisée.
+typedef struct Polynomial
+{
+    int lenght, deg;
+    Polynomial_dev developed, integrated, derivative;
+    Polynomial_fact factored;
+}Polynomial;
 
 
 // Affecte les réels x et y au champs x et y du Complex z.
